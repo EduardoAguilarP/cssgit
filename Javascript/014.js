736919387b46fd615b116af95344af5f7096e52f
eduardo <eduardo@gmail.com>
2022-11-02 13:51:45 +0100

    java actualizado

---
+++ Javascript/014.js
@@ -0,0 +1,12 @@
+var nota=2.6
+if(nota>=0 && nota<=3){
+    console.log("muy deficiente");
+} else if (nota>3 && nota<5){
+    console.log("insuficiiente");
+} else if (nota>=5 && nota<7){
+    console.log("bien");
+} else if (nota>=7 && nota<9){
+    console.log("notable");
+} else{
+    console.log("sobresaliente")
+}
