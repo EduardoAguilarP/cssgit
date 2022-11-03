1b083de223261c5d6a068f1701df9e8dc4ac97e0
eduardo <eduardo@gmail.com>
2022-11-03 13:38:28 +0100

    notaciones java

---
+++ Javascript/025.js
@@ -0,0 +1,12 @@
+var linea=""
+for(var i=0;i<=10;i=i+1){
+    if (i==0){
+        linea=linea+"("+i;        
+    }else if (i==10){
+        linea=linea+","+i+")";
+    }
+    else {
+        linea=linea+","+i;
+    }    
+}
+console.log(linea);
