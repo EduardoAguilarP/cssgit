c3462e1ae3636f7dcb38252749b39ae83bd52476
eduardo <eduardo@gmail.com>
2022-11-03 09:29:15 +0100

    java nuevo

---
+++ Javascript/015.js
@@ -0,0 +1,10 @@
+var importe=80;
+if(importe>0 && importe<100){
+    if(importe>0 && importe<50){
+        console.log("es barato");
+    }else{
+        console.log("es caro");
+    }
+} else {
+    console.log ("es carisimo");
+}
