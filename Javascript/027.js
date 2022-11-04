1be6f3b307208e74dad5b68d4f4fe370577b2bf9
eduardo <eduardo@gmail.com>
2022-11-04 10:15:32 +0100

    asteriscos

---
+++ Javascript/027.js
@@ -0,0 +1,9 @@
+var i=0;
+//acumular la suma
+//segun vamos dando vueltas
+var suma=0;
+for(var i=0;i<=5;i=i+1){
+    suma=suma+i;
+    console.log(i);
+}
+console.log(suma);
---
+++ Javascript/029.js
@@ -0,0 +1,5 @@
+var suma="*";
+for(var i=0;i<5;i=i+1){
+    console.log(suma);
+    suma=suma+"*"
+}
