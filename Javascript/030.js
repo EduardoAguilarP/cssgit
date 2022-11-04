d77053fba03e1b6aacd810071a193f4ef948f2d9
eduardo <eduardo@gmail.com>
2022-11-04 12:40:51 +0100

    asteriscos nuevo

---
+++ Javascript/030.js
@@ -0,0 +1,7 @@
+var texto1="*";
+var texto2=" *";
+
+for (i=0;i<10;i=i+1){
+    console.log(texto1);
+    console.log(texto2);
+}
---
+++ Javascript/032.js
@@ -0,0 +1,13 @@
+var texto1="*";
+var texto2=" *";
+var texto3="  *";
+var texto4="   *";
+var texto5="    *";
+
+for (i=0;i<5;i=i+1){
+    console.log(texto1);
+    console.log(texto2);
+    console.log(texto3);
+    console.log(texto4);
+    console.log(texto5);
+}
