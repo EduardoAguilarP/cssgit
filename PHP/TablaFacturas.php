ed3cd02c015d7359fea340a9bfd8ae77fe3b482e
eduardo <eduardo@gmail.com>
2022-11-11 13:50:32 +0100

    php nuevo

---
+++ PHP/TablaFacturas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <table>
+            <td>
+                <tr>1</tr>
+                <tr>ordenador</tr>
+                <tr>200</tr>
+                <tr>EmpresaA</tr>
+            </td>
+        </table>
+
+    </body>
+</html>
