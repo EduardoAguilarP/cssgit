9e6b1e36e6717d9e81c719fce339defbb2fdedd9
eduardo <eduardo@gmail.com>
2022-11-17 13:53:38 +0100

    formularios en php

---
+++ PHP/basico/verinformatico.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <?php if ($_GET["informatico"]=="on") {?>
+         Soy informatico
+        <?php } else { ?>
+        No lo soy 
+        <?php }?>
+    </body>
+</html>
